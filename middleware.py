8aaac52f043f0896e40039b583050045705a941e
Ronny Schneeberger <ronny.schneeberger@students.fhnw.ch>
2022-12-26 10:35:42 +0100

    setup middleware.py and requirements.txt

---
+++ middleware.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, jsonify, render_template, redirect, url_for, make_response
+from flask_cors import CORS, cross_origin
+
+import json
+from datetime import datetime
+import requests
+import os
+
+import math
+import base64
+import numpy as np
+import io
+
+# load env
+with open('secrets.env', 'r') as f:
+    secrets = json.load(f)
+
+print(secrets)

02e1d23c6d953dba8161b800c99ebaa4296e9b54
Ronny Schneeberger <ronny.schneeberger@students.fhnw.ch>
2023-01-13 13:37:03 +0100

    changed push to azure

--- azure-containerized-albert/push_to_azure.sh
+++ azure-containerized-albert/push_to_azure.sh
@@ -2,5 +2,5 @@
 
 az login
 az acr login --name d0fc41ec9cbe4bdebd01d7b30df6209b
-docker tag mock d0fc41ec9cbe4bdebd01d7b30df6209b.azurecr.io/
+docker tag azure-containerized-albert d0fc41ec9cbe4bdebd01d7b30df6209b.azurecr.io/
 docker push d0fc41ec9cbe4bdebd01d7b30df6209b.azurecr.io/azure-containerized-albert
